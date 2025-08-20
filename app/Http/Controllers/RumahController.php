d9f33ed392484c1e096a94e9034f92f1e8983d1f
nindria <netania.indria@gmail.com>
2025-08-21 01:36:22 +0700

    layout datawarga

--- app/Http/Controllers/RumahController.php
+++ app/Http/Controllers/RumahController.php
@@ -14,6 +14,7 @@ class RumahController extends Controller
      */
     public function index()
     {
+        
         $rumahs = Rumah::latest()->paginate(10);
         return Inertia::render('Rumah/Index', [
             'rumahs' => $rumahs
